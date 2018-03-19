ef8ddf521ab1a3c698964d1d105f0abcef52faac
Bartłomiej Karaban <bkaraban@kruk-inkaso.com.pl>
2018-03-19 14:57:15 +0100

    Dodanie do repozytorium.

---
+++ laboratorium 5 rozwiązania.R
@@ -0,0 +1,49 @@
+
+
+library(rpart)
+library(rpart.plot)
+library(ada)
+library(gbm)
+library(randomForest)
+library(caret)
+
+## Zadanie 1
+
+
+# Wykonaj samodzielnie (bez u¿ywania dedykowanych pakietów/funkcji) wykres ROC. 
+# Wejœciem bêdzie wektor parwdopodobieñstw i wektor oznaczeñ good/bad.
+
+
+
+
+
+## Zadanie 2
+
+# Zdefiniuj zmienn¹ celu jako pojawienie siê wp³aty w pierwszych 6ciu miesiêcy obs³ugi. Nastêpnie podziel zbiór danych 
+# na ucz¹cy i testowy w proporcji 70% i 30% zachwouj¹c taki sam rozk³ad zmiennej celu w zbiorze ucz¹cycm i testowym. 
+# Do wykonania zadania u¿yj funkcji createDataPartition z pakietu caret
+
+## Zadanie 3
+
+# U¿yj drzewa klasyfikacyjnego do modelowania zjawiska czy w sprawie pojawi siê 
+# wp³ata w ci¹gu pierwszych 6 miesiêcy obs³ugi. Zadanie wykonaj wykorzystuj¹c
+# pakiety tree i rpart.
+
+## Zadanie 4
+
+# Zbuduj drzewo regresyjne do modelowania skutecznoœci na sprawie od 4 do 12 
+# miesi¹ca obs³ugi (Wsk. Mo¿esz wykorzystaæ zmienne behawioralne z pierwszych
+#                   trzech miesiêcy obs³ugi).
+
+## Zadanie 5
+
+# Zbuduj drzewa regresyjne do modelowania skutecznoœci skumulowanej na sprawie w 12 mieisi¹cu obs³ugi
+# dla wszystkich kombinacji wartoœci parametrów .....(Wskazówka: do zbudowania siatki kombinacji u¿yj 
+# funkcji expand.grid). Nastêpinie porównaj b³¹d RMSE dla ka¿dego z drzew na zbiorze trainingowym i tesotwym.
+
+
+## Zadanie 6
+
+#  U¿yj lasu losowego do modelowania zjawiska pojawienia siê wp³aty w ci¹gu 6 miesiêcy obs³ugi. Wyniki
+# pórównaj z usyzskanymi w zadaniu 3.
+
