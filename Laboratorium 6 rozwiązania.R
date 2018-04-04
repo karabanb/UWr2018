8d4dde23b0320df312c540f9c2a85fbbd7d4f361
Bartłomiej Karaban <bkaraban@kruk-inkaso.com.pl>
2018-04-04 12:58:18 +0200

    rozwiązanie zadania 1

--- Laboratorium 6 rozwiązania.R
+++ Laboratorium 6 rozwiązania.R
@@ -1,10 +1,47 @@
 
+
+library(data.table)
+
+
+load("KrukUWr2018.RData")
+
 ### zadanie 1 #####
 
 # Przeanalizuj wartości `NA` na zmiennej `LoanAmount`. Czy wszystkie wartości `NA` 
 # w przypadku tej zmiennej oznaczają brak danych? 
 
 
+
 summary(cases$LoanAmount)
 
-head(cases[is.na(LoanAmount),]) #
+# jak można zauwazyć problem dotyczy głownie kart
+cases[,.(.N), by = .(Product, is.na(LoanAmount))][order(Product)] 
+
+# na pierwszy rzut oka wartość Principal jest równa LoanAmount
+head(cases[!is.na(LoanAmount) & Product == "Credit card", 1:7])
+
+# porównajmy statystyki opisowe
+
+summary(cases[!is.na(LoanAmount) & Product == "Credit card" , LoanAmount])
+summary(cases[!is.na(LoanAmount) & Product == "Credit card" , Principal])
+
+# tak wyglada to wizualnie
+
+plot(density(cases[!is.na(LoanAmount) & Product == "Credit card" , LoanAmount]))
+lines(density(cases[!is.na(LoanAmount) & Product == "Credit card" , Principal]), col = "red")
+
+# po odcięciu najwyższych 10%
+
+quantile(cases[!is.na(Principal),Principal], probs = seq(0, 1, 0.05))
+
+plot(density(cases[Product == "Credit card" & LoanAmount < 20000  , LoanAmount],na.rm = TRUE))
+lines(density(cases[Product == "Credit card" & LoanAmount < 20000, Principal], na.rm = TRUE), col = "red")
+
+
+#### w przypadku kart kredytowych wartośc LoanAmount możemy zastapić Principal #####
+
+
+#### Zadanie 2 ######
+
+
+
