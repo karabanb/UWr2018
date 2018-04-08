1fe8f35d56efca5760999a9bcbaad272ac8ce357
karabanb <b.karaban@me.com>
2018-04-08 23:23:26 +0200

    dodanie samplowania z rozkladu do zadania 3

--- Laboratorium 6 rozwiązania.R
+++ Laboratorium 6 rozwiązania.R
@@ -42,6 +42,7 @@ lines(density(cases[Product == "Credit card" & LoanAmount < q90, Principal], na.
 cases[Product == "Credit card" & is.na(LoanAmount), LoanAmount := Principal]
 anyNA(cases[Product == "Credit card", LoanAmount]) # sprawdzamy czy w tym zbiorze zostaly NA
 
+
 #### Zadanie 2 ######
 
 # Podaj biznesowy (ekspercki) sposób uzupełnienia wartości `NA` dla zmiennej `Other`.
@@ -55,5 +56,31 @@ anyNA(cases[,Other])
 
 #### Zadanie 3 ######
 
+# Uzupełnij braki danych na zmiennej `Land` wykorzystując rozkład empiryczny. 
+# Jak wykorzystać nowo pozyskane informacje w uzupełnieniu zmiennych `GDPPerCapita` i `MeanSalary`?
 
+
+land_distribution <- prop.table(table(cases$Land)) #Pamietajmy, ze Land to factor!
+
+barplot(land_distribution) #wizualizacja
+
+# probkowanie z rozkladu Land
+
+sampled <- sample(land_distribution$V1,
+                  size = sum(is.na(cases$Land)),
+                  prob = land_distribution$N,
+                  replace = TRUE)
+
+cases[is.na(Land), Land:= as.integer(sampled)] # zastapienie brakow danych 
+
+anyNA(cases$Land) #nie ma juz NA's
+
+# Prosze zauwazyc, ze kazdy Land ma przypisana 1 wartosc MeanSalary i GDPPerCapita, 
+# przyklad: wartosc min i max dla interesujacych nas cech jest taka sama
+
+cases[!is.na(MeanSalary), .(unqMeanSalry = unique(MeanSalary, na.rm =T))
+          # maxMeanSalry = max(MeanSalary, na.rm = T),
+          # minGDPPerCapita = min(GDPPerCapita, na.rm =T),
+          # maxGDPPerCapita = max(GDPPerCapita, na.rm = T)),
+          , by = Land]
 
