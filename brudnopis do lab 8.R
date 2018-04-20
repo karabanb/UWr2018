1283572f88e49c4f3c84e8dad0287ebec636d447
Bartłomiej Karaban <bkaraban@kruk-inkaso.com.pl>
2018-04-20 13:18:35 +0200

    modyfikacja poleceń i rozwiazan

--- brudnopis do lab 8.R
+++ brudnopis do lab 8.R
@@ -8,7 +8,7 @@
 ### liczymy TSS
 ### Liczymy R^2
 
-cases_loanamount_nas <- cases[is.na(LoanAmount)&Product=="Cash loan",]
+cases_loanamount <- cases[Product=="Cash loan",]
 
 # przygotuj ramke danych 'cases_loanamount_nas' bazując na tabeli 'cases', która zawieraja
 # wszytskie przypadki brakujacych danych zmiennej 'LoanAmount' dla kredytów gotówkowych 
@@ -16,7 +16,18 @@ cases_loanamount_nas <- cases[is.na(LoanAmount)&Product=="Cash loan",]
 
 ## sprawdz ile brakow danych wystepuje w innych zmiennych i ewentualnie zaproponuj metode i zastap te braki, 
 
-sapply(cases_loanamount_nas, function(x){sum(is.na(x))})
+sapply(cases_loanamount, function(x){sum(is.na(x))})
+
+# Lan z rozkladu
+
+land_distribution <- prop.table(table(cases_loanamount$Land))
+
+sampled <- sample(unique(cases_loanamount[!is.na(Land),Land]),
+                  size = sum(is.na(cases_loanamount$Land)),
+                  prob = land_distribution,
+                  replace = TRUE)
+
+
 
 
 cases_loanamount_nas[is.na(Other), Other:=TOA-Principal-Interest]
